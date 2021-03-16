076a0d79c1f65553e602b747240cab0b4d6a2f61
Stefan Touhami <Stefan@Touhami.pl>
2021-03-16 14:01:01 +0100

    Add Doctrine ORM

--- src/Kernel.php
+++ src/Kernel.php
@@ -14,19 +14,19 @@ class Kernel extends BaseKernel
     protected function configureContainer(ContainerConfigurator $container): void
     {
         $container->import('../config/{packages}/*.php');
-        $container->import('../config/{packages}/'.$this->environment.'/*.php');
+        $container->import('../config/{packages}/' . $this->environment . '/*.php');
 
-        if (is_file($path = \dirname(__DIR__).'/config/services.php')) {
+        if (is_file($path = \dirname(__DIR__) . '/config/services.php')) {
             (require $path)($container->withPath($path), $this);
         }
     }
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/'.$this->environment.'/*.php');
+        $routes->import('../config/{routes}/' . $this->environment . '/*.php');
         $routes->import('../config/{routes}/*.php');
 
-        if (is_file($path = \dirname(__DIR__).'/config/routes.php')) {
+        if (is_file($path = \dirname(__DIR__) . '/config/routes.php')) {
             (require $path)($routes->withPath($path), $this);
         }
     }
